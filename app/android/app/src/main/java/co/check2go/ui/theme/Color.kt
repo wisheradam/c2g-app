0fc1eabce521b7d9bea3ed196751cebc41a14113
Adam Wisher <adam.wisher@innova.co>
2026-09-13 19:53:17 +0300

    Android bootstrap: add Check2GO color tokens

---
+++ app/android/app/src/main/java/co/check2go/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package co.check2go.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val C2GPrimary = Color(0xFF043CB3)
+val C2GLightTextPrimary = Color(0xFF002349)
+val C2GTextSecondary = Color(0xFF8D919A)
+val C2GLightBackground = Color(0xFFF4F7FA)
+val C2GError = Color(0xFFB3261E)
+
+val C2GDarkTextPrimary = Color(0xFFD0D0D0)
+val C2GDarkDisabledSurface = Color(0xFF3A4F66)
+val C2GDarkBackground = Color(0xFF253C55)
